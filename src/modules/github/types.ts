ee91cce7f6c1a820875189977e62f001a7aabb70
kbu715 <kbu715@gmail.com>
2021-11-27 00:29:04 +0900

    ♻️ refactor: 리듀서 리팩토링하기

--- src/modules/github/types.ts
+++ src/modules/github/types.ts
@@ -1,13 +1,23 @@
 import * as actions from './actions';
 import { ActionType } from 'typesafe-actions';
 import { GithubProfile } from '../../lib/api/github';
+import { AsyncState } from '../../lib/reducerUtils';
 
 export type GithubAction = ActionType<typeof actions>;
 
+// export type GithubState = {
+//   userProfile: {
+//     loading: boolean;
+//     error: Error | null;
+//     data: GithubProfile | null;
+//   };
+// };
+
 export type GithubState = {
-  userProfile: {
-    loading: boolean;
-    error: Error | null;
-    data: GithubProfile | null;
-  };
+  userProfile: AsyncState<GithubProfile, Error>;
 };
+
+/*
+AsyncState 를 사용해서 매번 loading, data, error 의 타입을 
+직접 입력해줄 필요 없이 한줄로 깔끔하게 작성 할 수 있게 됐습니다.
+*/
